d055df028bf2c3d2e410fdc740bfd68071cb9b86
gak92 <44802922+gak92@users.noreply.github.com>
2021-01-06 01:41:10 +0500

    Time: 4 ms (17.50%), Space: 10.1 MB (18.03%) - LeetHub

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,15 +1,41 @@
 class Solution {
+    vector<vector<int>> dp;
 public:
+    Solution(): dp(101, vector<int>(101,-1)){}
+    
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n,1)); //initialize with 1
+        if(m<1 || n<1)          // check out of bound
+            return 0;
+        if(m==1 && n==1)        // check first row and first column
+            return 1;
         
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                
-            }
-        }
+        if(dp[m][n] != -1)      // if already calculated
+            return dp[m][n];  
         
-        return dp[m-1][n-1];
+        dp[m][n] = uniquePaths(m,n-1)+uniquePaths(m-1,n);
+        
+        return dp[m][n];
     }
 };
+​
+​
+​
+​
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m, vector<int>(n,1)); //initialize with 1
+        
+//         for(int i=1; i<m; i++){
+//             for(int j=1; j<n; j++){
+//                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                
+//             }
+//         }
+        
+//         return dp[m-1][n-1];
+//     }
+// };
+​
+​
+​
