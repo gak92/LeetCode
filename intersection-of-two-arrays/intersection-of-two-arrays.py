06f95bbe7d3b6f4f667dd593b4b1f196d3f95984
gak92 <44802922+gak92@users.noreply.github.com>
2021-03-13 13:38:22 +0500

    Time: 40 ms (92.93%), Space: 14.3 MB (91.74%) - LeetHub

--- intersection-of-two-arrays/intersection-of-two-arrays.py
+++ intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,17 +1,33 @@
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        intersect = []
-        l1 = len(nums1)
-        l2 = len(nums2)
-        if l1 < l2:
-            smallest = set(nums1)
-            largest = set(nums2)
-        else:
-            smallest = set(nums2)
-            largest = set(nums1)
-​
-        for val in smallest:
-            if (val in largest):
-                intersect.append(val)
-        return intersect
-​
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        dic = {}
+        res = []
+        
+        for num in nums1:
+            dic[num] = 1
+            
+        for num in nums2:
+            if num in dic:
+                dic[num] = 2
+                
+        for num in dic:
+            if dic[num] == 2:
+                res.append(num)
+            
+        return res
+        
+        
+#         intersect = []
+#         l1 = len(nums1)
+#         l2 = len(nums2)
+#         if l1 < l2:
+#             smallest = set(nums1)
+#             largest = set(nums2)
+#         else:
+#             smallest = set(nums2)
+#             largest = set(nums1)
+
+#         for val in smallest:
+#             if (val in largest):
+#                 intersect.append(val)
+#         return intersect
