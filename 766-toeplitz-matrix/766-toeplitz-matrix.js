3fe6d3d6101037cb522d94c670e11738b81f1424
Ghazanfar Ali <gak_92@hotmail.com>
2022-09-26 00:57:24 +0500

    Time: 129 ms (13.99%), Space: 44.3 MB (37.20%) - LeetHub

---
+++ 766-toeplitz-matrix/766-toeplitz-matrix.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var isToeplitzMatrix = function(matrix) {
+    for (let i=0; i<matrix.length; i++) {
+        for (let j=0; j<matrix[0].length; j++) {
+            if (i>0 && j>0 && matrix[i][j] !== matrix[i-1][j-1])
+                return false;
+        }
+    }
+    return true;
+    
+};
