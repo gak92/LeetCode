63e0490f551ef2472793841d395ceb7dceb2374c
Ghazanfar Ali <gak_92@hotmail.com>
2022-09-27 12:56:13 +0500

    Time: 86 ms (62.13%), Space: 42.6 MB (14.93%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.js
@@ -4,26 +4,36 @@
  */
 var getRow = function(rowIndex) {
     
-//     let currRow = [];
-//     let prevRow = [];
+    let res = [];
     
-//     for (let i=0; i<rowIndex; i++) {
+    for (let i=0; i<=rowIndex; i++) {
+        res.push([]);
         
+        for (let j=0; j<=i; j++) {
+            if (j==0 || j==i) {
+                res[i][j] = 1;
+            }
+            else {
+                res[i][j] = res[i-1][j-1] + res[i-1][j];
+            }
+        }
+    }
+    return res[rowIndex];
+    
+    
+//     let currRow = [];
+//     currRow.push(1);
+//     if (rowIndex == 0) {
+//         return [1];
 //     }
     
-    let currRow = [];
-    currRow.push(1);
-    if (rowIndex == 0) {
-        return [1];
-    }
+//     let prev = getRow(rowIndex - 1);
     
-    let prev = getRow(rowIndex - 1);
+//     for (let i=1; i<prev.length; i++) {
+//         let curr = prev[i-1] + prev[i];
+//         currRow.push(curr);
+//     }
+//     currRow.push(1);
     
-    for (let i=1; i<prev.length; i++) {
-        let curr = prev[i-1] + prev[i];
-        currRow.push(curr);
-    }
-    currRow.push(1);
-    
-    return currRow;
+//     return currRow;
 };
