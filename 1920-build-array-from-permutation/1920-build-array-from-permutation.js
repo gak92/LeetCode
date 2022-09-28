ea6f5e3530e0f9a822ebe29ea87d8ddf3adfb3c1
Ghazanfar Ali <gak_92@hotmail.com>
2022-09-28 16:00:06 +0500

    Time: 160 ms (10.55%), Space: 45.1 MB (90.78%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.js
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -3,11 +3,22 @@
  * @return {number[]}
  */
 var buildArray = function(nums) {
-    let ans = [];
-    for (let i=0; i<nums.length; i++) {
-        ans[i] = nums[nums[i]];
+//     let ans = [];
+//     for (let i=0; i<nums.length; i++) {
+//         ans[i] = nums[nums[i]];
+//     }
+    
+//     return ans;
+    let len = nums.length;
+    
+    for (let i=0; i<len; i++) {
+        nums[i] = len * (nums[nums[i]] % len) + nums[i];
     }
     
-    return ans;
+    for (let i=0; i<len; i++) {
+        nums[i] = Math.floor(nums[i] / len);
+    }
+    
+    return nums;    
     
 };
