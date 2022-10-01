a0524c7dab05aa202b9d239ed249daf2c5d565e7
Ghazanfar Ali <gak_92@hotmail.com>
2022-10-01 13:24:43 +0500

    Time: 124 ms (8.43%), Space: 44.7 MB (5.76%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,16 +3,21 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let res = [];
+//     let res = [];
     
-    for(let i=0; i<nums.length; i++) {
-        if(i==0) {
-            res.push(nums[i]);
-        }
-        else{
-            res.push(nums[i] + res[i-1]);    
-        }
+//     for(let i=0; i<nums.length; i++) {
+//         if(i==0) {
+//             res.push(nums[i]);
+//         }
+//         else{
+//             res.push(nums[i] + res[i-1]);    
+//         }
+//     }
+    
+    for(let i=1; i<nums.length; i++) {
+        nums[i] += nums[i-1];
     }
-    console.log(res);
-    return res;
+    console.log(nums);
+
+    return nums;
 };
