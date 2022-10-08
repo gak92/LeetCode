482d39842e177528a67c59aa9269ce4d84c857f4
Ghazanfar Ali <gak_92@hotmail.com>
2022-10-08 14:35:03 +0500

    Time: 127 ms (25.39%), Space: 42.5 MB (33.11%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function(nums, diff) {
+    let count = 0;
+    
+    for(let i=0; i<nums.length; i++)
+    {
+        for(let j=i+1; j<nums.length; j++)
+        {
+            if(nums[j]-nums[i] == diff)
+            {
+                for(let k=j+1; k<nums.length; k++)
+                {
+                    if(nums[k]-nums[j] == diff)
+                    {
+                        count += 1;
+                    }
+                }
+            }
+        }
+    }
+    
+    console.log(count);
+    return count;
+};
