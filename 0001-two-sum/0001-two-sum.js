35461966bb2ecc5b3682d8e9f436402ad1290114
Ghazanfar Ali <gak_92@hotmail.com>
2022-10-11 13:36:24 +0500

    Time: 115 ms (62.31%), Space: 51.2 MB (5.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,17 +5,45 @@
  */
 var twoSum = function(nums, target) {
     
+    let complement = {};
+    
+    for(let i=0; i<nums.length; i++)
+    {
+        complement[nums[i]] = i;
+    }    
+    console.log(complement);
+    for(let i=0; i<nums.length; i++)
+    {
+        let x = target - nums[i];
+        if(x in complement && i!== complement[x])
+        {
+            return [i, complement[x]]
+        }
+    }
+    
+    
+    // for(let i=0; i<nums.length-1; i++)
+    // {
+    //     let x = nums[i];
+    //     for(let j=i+1; j<nums.length; j++)
+    //     {
+    //         if(nums[j] === target-x)
+    //         {
+    //             return [i,j];
+    //         }
+    //     }
+    // }
     
     
 //     Brute force approach
-    for(let i=0; i<nums.length-1; i++)
-    {
-        for(let j=i+1; j<nums.length; j++)
-        {
-            if((nums[i]+nums[j]) === target)
-            {
-                return [i, j];
-            }
-        }
-    }
+    // for(let i=0; i<nums.length-1; i++)
+    // {
+    //     for(let j=i+1; j<nums.length; j++)
+    //     {
+    //         if((nums[i]+nums[j]) === target)
+    //         {
+    //             return [i, j];
+    //         }
+    //     }
+    // }
 };
