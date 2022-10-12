8228bdcf252aa75d9ebacc3585c0e2e17d8e848e
Ghazanfar Ali <gak_92@hotmail.com>
2022-10-12 19:10:30 +0500

    Time: 154 ms (34.44%), Space: 47 MB (57.44%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.js
+++ 2418-sort-the-people/2418-sort-the-people.js
@@ -5,24 +5,31 @@
  */
 var sortPeople = function(names, heights) {
     
-    for(let i=0; i<names.length; i++)
-    {
-        // let max = heights[i];
-        // let maxidx = i;
-        for(let j=i+1; j<heights.length; j++)
-        {
-            if(heights[j]>heights[i])
-            {
-                let temp = heights[j];
-                heights[j] = heights[i];
-                heights[i] = temp;
-                let temp2 = names[j];
-                names[j] = names[i];
-                names[i] = temp2;
-            }
-        }
-        
-    }
+    const map = new Map();
+    let res = [];
     
-    return names;
+    heights.forEach((height, i) => map.set(height, names[i]));
+    heights.sort((a,b) => b-a);
+    heights.forEach((height) => res.push(map.get(height)));
+    
+    return res;
+    
+//     for(let i=0; i<names.length; i++)
+//     {
+//         for(let j=i+1; j<heights.length; j++)
+//         {
+//             if(heights[j]>heights[i])
+//             {
+//                 let temp = heights[j];
+//                 heights[j] = heights[i];
+//                 heights[i] = temp;
+//                 let temp2 = names[j];
+//                 names[j] = names[i];
+//                 names[i] = temp2;
+//             }
+//         }
+        
+//     }
+    
+    // return names;
 };
