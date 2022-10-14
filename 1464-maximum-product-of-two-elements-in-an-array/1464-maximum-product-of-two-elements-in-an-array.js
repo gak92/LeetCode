63a03e6750bc6da90e8cbc7bf3d05f789645779e
Ghazanfar Ali <gak_92@hotmail.com>
2022-10-14 12:26:39 +0500

    Time: 108 ms (44.62%), Space: 47.1 MB (5.38%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -4,7 +4,26 @@
  */
 var maxProduct = function(nums) {
     
-    nums.sort((a,b)=>b-a);
+    for(let i=0; i<nums.length-1; i++)
+    {
+        for(let j=i; j<nums.length; j++)
+        {
+            if(nums[i] > nums[j])               
+            {
+                let temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
     
-    return ((nums[0]-1) * (nums[1]-1));
+    
+    
+    console.log(nums);
+    
+    return ((nums[nums.length-1]-1) * (nums[nums.length-2] -1))
+    
+    
+    // nums.sort((a,b)=>b-a);
+    // return ((nums[0]-1) * (nums[1]-1));
 };
