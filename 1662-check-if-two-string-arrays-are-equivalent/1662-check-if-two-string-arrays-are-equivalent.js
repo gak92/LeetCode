1836ae43d4899d385cf60a50ab71b0fa65c6bd18
Ghazanfar Ali <gak_92@hotmail.com>
2022-10-30 12:11:05 +0500

    Time: 109 ms (19.26%), Space: 42.1 MB (55.79%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    let s1 = '';
+    let s2 = '';
+    
+    for(let w of word1)
+    {
+        s1 += w;   
+    }
+    for(let w of word2)
+    {
+        s2 += w;
+    }
+
+    
+    if(s1 === s2)
+        return true;
+    else
+        return false;
+    
+};
