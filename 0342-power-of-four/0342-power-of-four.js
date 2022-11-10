a7bafd8172ea7592b0c545ca592992c4fcc71f50
Ghazanfar Ali <gak_92@hotmail.com>
2022-11-10 12:27:30 +0500

    Time: 110 ms (66.20%), Space: 43.8 MB (26.56%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    // n=16 ===>   16/4 ===> 4/4  ===> 1
+    // n=5 ===> 5/4   ===> 1.21, 5//4 ==> 1 ===> 4 ===> 0
+    
+    if(n===0)
+        return false;
+    
+    if(n===1)
+        return true;
+    
+    while(n % 4 === 0)          // 16 % 4 ===> 4, 4%4 ===> 0, 5 % 4 ==> 1, 1..2%4 ==> 0
+    {
+        n = n / 4;              // 16/4 ===> 4 ,    4/4 = 1, 5/4 ==>1.222
+    }
+    
+    if(n === 1)
+        return true;
+    else
+        return false;
+    
+};
