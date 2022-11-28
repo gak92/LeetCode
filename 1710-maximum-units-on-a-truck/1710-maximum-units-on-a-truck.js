d5445847d0927fb2e2c782c8bc61c3fbfae467b7
Ghazanfar Ali <gak_92@hotmail.com>
2022-11-28 23:25:47 +0500

    Time: 130 ms (51.44%), Space: 45.2 MB (28.16%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.js
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.js
@@ -5,13 +5,13 @@
  */
 var maximumUnits = function(boxTypes, truckSize) {
     boxTypes.sort((a,b) => b[1]-a[1]);
-    console.log(boxTypes);
+    // console.log(boxTypes);
     
     let totalUnits = 0;
     for(let box of boxTypes)
     {
         let numOfBoxes = Math.min(truckSize, box[0]);
-        console.log("Min val is: ", numOfBoxes);
+        // console.log("Min val is: ", numOfBoxes);
         totalUnits += numOfBoxes * box[1];
         truckSize -= numOfBoxes;
     }
