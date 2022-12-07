8654a81744832b206f8034de72438417c45f6f1c
Ghazanfar Ali <gak_92@hotmail.com>
2022-12-07 20:25:14 +0500

    Create 206. Reverse Linked List.js

---
+++ 206. Reverse Linked List.js
@@ -0,0 +1,30 @@
+/*
+206. Reverse Linked List
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let currentNode = head;
+    let previousNode = null;
+    let nextNode;
+
+    while(currentNode !== null) {
+        nextNode = currentNode.next;
+        currentNode.next = previousNode;
+        previousNode = currentNode;
+        currentNode= nextNode;
+    }
+    return previousNode;
+};
